9579589a19db3efdbd11e85eb1d1412119b5c523
James Bester <jamesthomasbester@gmail.com>
2022-05-29 19:05:11 +1000

    nav

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homePage = require('./homePage');
+const dashboard = require('./dashboard');
 
-router.use('/', homePage);
+router.use('/', dashboard);
 
 module.exports = router;
